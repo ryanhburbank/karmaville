45af6aef5c84cf0b1177df881fe0acd192202f7a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-12 13:55:02 -0500

    Add index to karma point user id

--- db/migrate/20130812153138_add_index_to_karma_points.rb
+++ db/migrate/20130812153138_add_index_to_karma_points.rb
@@ -2,5 +2,6 @@ class AddIndexToKarmaPoints < ActiveRecord::Migration
   def change
     add_index :karma_points, :value
     add_index :karma_points, :label
+    add_index :karma_points, :user_id
   end
 end
