993fd03255ac8103d6eb2b137e317c93fd9900f4
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-12 13:54:16 -0500

    Updates total karma points in User table when a new KarmaPoint is created

--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -2,7 +2,16 @@ class KarmaPoint < ActiveRecord::Base
   attr_accessible :user_id, :label, :value
   belongs_to :user
 
+  after_save :total_karma_update
+
   validates :user, :presence => true
   validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   validates :label, :presence => true
+
+  private
+
+  def total_karma_update
+    user = User.find(self.user_id)
+    user.update_attribute(:total_karma_points, user.sum_karma)
+  end
 end
